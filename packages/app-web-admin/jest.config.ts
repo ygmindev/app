64569e439090a12bb7d02f2827953d70d285026f
YG Min <ygminceo@gmail.com>
2022-10-02 00:01:26 -0400

    commit

--- packages/app-web-admin/jest.config.ts
+++ packages/app-web-admin/jest.config.ts
@@ -1,1 +1,1 @@
-export { jestConfig as default } from '@lib/config/jest/jest/jest.base.config';
+export { jestConfig as default } from '@lib/config/jest/jest/jest.web.config';
--- packages/app-web-admin/tasks.config.ts
+++ packages/app-web-admin/tasks.config.ts
@@ -1,5 +1,10 @@
+import { APP_NAME } from '@app/web-admin/app/app.constants';
+import { register } from '@tool/task/core/utils/register/register';
+import { dev } from '@tool/task/cra/tasks/dev/dev';
 import { registerNodeTasks } from '@tool/task/node/utils/registerNodeTasks/registerNodeTasks';
 
 import { name } from './package.json';
 
 registerNodeTasks({ name });
+
+register({ ...dev, options: { name: APP_NAME }, target: name });
