a14a6501c7ced695e1e987b3851aa6fc0d61e215
YG Min <ygminceo@gmail.com>
2022-11-20 11:12:34 -0500

    commit

--- packages/lib-backend/src/billing/resources/Card/Card.ts
+++ packages/lib-backend/src/billing/resources/Card/Card.ts
@@ -1,9 +1,9 @@
 import { withEntity } from '@lib/backend/resource/decorators/withEntity/withEntity';
 import { withField } from '@lib/backend/resource/decorators/withField/withField';
-import { FIELD_TYPE } from '@lib/shared/form/form.constants';
 import { EmbeddedResource } from '@lib/backend/resource/resources/EmbeddedResource/EmbeddedResource';
 import { CARD_RESOURCE_NAME } from '@lib/shared/billing/resources/Card/Card.constants';
 import type { CardModel, CardTypeModel } from '@lib/shared/billing/resources/Card/Card.models';
+import { FIELD_TYPE } from '@lib/shared/form/form.constants';
 
 @withEntity({ isEmbedded: true, isRepository: true, name: CARD_RESOURCE_NAME })
 export class Card extends EmbeddedResource implements CardModel {
--- packages/lib-frontend/src/billing/containers/Cards/Cards.constants.tsx
+++ packages/lib-frontend/src/billing/containers/Cards/Cards.constants.tsx
@@ -1,19 +1,30 @@
+import { MaskedTextField } from '@lib/frontend/form/components/MaskedTextField/MaskedTextField';
 import { FORM_FIELD_TYPE } from '@lib/frontend/form/containers/FormContainer/FormContainer.constants';
 import { ENTITY_RESOURCE_COLUMNS } from '@lib/frontend/resource/components/Resources/Resources.constants';
 import type { ResourcesPropsModel } from '@lib/frontend/resource/components/Resources/Resources.models';
 import { CARD_RESOURCE_NAME } from '@lib/shared/billing/resources/Card/Card.constants';
 import type { CardFormModel, CardModel } from '@lib/shared/billing/resources/Card/Card.models';
-import { FIELD_TYPE } from '@lib/shared/form/form.constants';
 import type { UserModel } from '@lib/shared/user/resources/User/User.models';
 
 export const CARDS_PROPS: ResourcesPropsModel<CardModel, CardFormModel, UserModel> = {
   columns: [
     ...ENTITY_RESOURCE_COLUMNS.map((column) => ({ ...column, isHidden: true })),
+
     {
       field: FORM_FIELD_TYPE.TEXT_FIELD,
-      id: 'last4',
-      label: ({ t }) => t('billing:labels.last4'),
-      type: FIELD_TYPE.NUMBER,
+      id: 'name',
+      label: ({ t }) => t('billing:labels.nameOnCard'),
+    },
+
+    {
+      id: 'exp',
+      label: ({ t }) => t('billing:labels.expiration'),
+      render: (props) => (
+        <MaskedTextField
+          {...props}
+          mask="MM/YY"
+        />
+      ),
     },
   ],
 
