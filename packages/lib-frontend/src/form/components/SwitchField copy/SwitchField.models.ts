bef7047c97bf43550cf79b8e38c0aca192ae280b
YG Min <ygminceo@gmail.com>
2023-06-04 17:41:44 -0400

    commit

--- packages/lib-frontend/src/form/components/SwitchField copy/SwitchField.models.ts
+++
@@ -1,5 +0,0 @@
-import type { _SwitchFieldPropsModel } from '@lib/frontend/form/components/SwitchField/_SwitchField.models';
-import type { TranslatableFieldPropsModel } from '@lib/frontend/form/form.models';
-
-export interface SwitchFieldPropsModel
-  extends TranslatableFieldPropsModel<_SwitchFieldPropsModel> {}
--- packages/lib-frontend/src/form/components/SwitchField copy/_SwitchField.models.ts
+++
@@ -1,7 +0,0 @@
-import type { IconPropsModel } from '@lib/frontend/core/components/Icon/Icon.models';
-import type { StringFieldPropsModel } from '@lib/frontend/form/form.models';
-
-export interface _SwitchFieldPropsModel extends StringFieldPropsModel<'true' | 'false'> {
-  iconActive?: IconPropsModel['icon'];
-  iconInactive?: IconPropsModel['icon'];
-}
