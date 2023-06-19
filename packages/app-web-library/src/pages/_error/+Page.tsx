7d54bfae35975a49cb17d7f71bea8df650d195de
YG Min <ygminceo@gmail.com>
2023-06-19 13:22:13 -0400

    commit

--- packages/app-web-library/src/pages/_error/+Page.tsx
+++
@@ -1,5 +0,0 @@
-import { exportPage } from '#lib-platform/web/exports/exportPage/exportPage';
-
-const { Page } = exportPage({ Component: () => <>TODO: SSR ERROR PAGE</> });
-
-export default Page;
--- packages/app-web-library/src/renderer/+config.ts
+++
@@ -1,3 +0,0 @@
-import type { Config } from 'vite-plugin-ssr/types';
-
-export default { clientRouting: true, passToClient: ['pageProps', 'context'] } satisfies Config;
