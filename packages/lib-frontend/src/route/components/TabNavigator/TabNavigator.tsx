d05647146a862917369898fddf3ad55411fbe62d
YG Min <ygminceo@gmail.com>
2023-08-24 23:37:11 -0400

    commit

--- packages/lib-frontend/src/route/components/TabNavigator/TabNavigator.tsx
+++ packages/lib-frontend/src/route/components/TabNavigator/TabNavigator.tsx
@@ -1,9 +1,10 @@
+import { useEffect } from 'react';
+
 import { Tabs } from '#lib-frontend/core/components/Tabs/Tabs';
 import { type SFCModel } from '#lib-frontend/core/core.models';
 import { useTranslation } from '#lib-frontend/locale/hooks/useTranslation/useTranslation';
 import { type TabNavigatorPropsModel } from '#lib-frontend/route/components/TabNavigator/TabNavigator.models';
 import { useRouter } from '#lib-frontend/route/hooks/useRouter/useRouter';
-import { type RouteModel } from '#lib-frontend/route/route.models';
 import { useStyles } from '#lib-frontend/style/hooks/useStyles/useStyles';
 
 export const TabNavigator: SFCModel<TabNavigatorPropsModel> = ({
@@ -20,23 +21,14 @@ export const TabNavigator: SFCModel<TabNavigatorPropsModel> = ({
     isActive({ pathname: fullpath ?? pathname }),
   );
 
-  const getIndexRoute = (value?: string, childRoutes?: Array<RouteModel>): string | undefined => {
-    if (value) {
-      const route = childRoutes?.find(({ fullpath }) => fullpath === value);
-      return route?.isLeaf
-        ? route?.fullpath
-        : route?.routes
-        ? getIndexRoute(route.routes[0].fullpath, route.routes)
-        : undefined;
-    }
-    return undefined;
-  };
+  useEffect(() => {
+    !isActiveF && routes && void push({ pathname: routes[0].fullpath || routes[0].pathname });
+  }, [isActiveF]);
 
   return (
     <Tabs
-      onChange={(value) => {
-        const pathname = getIndexRoute(value, routes);
-        pathname && void push({ pathname });
+      onChange={(pathname) => {
+        void push({ pathname });
       }}
       style={styles}
       tabs={routes?.map(({ fullpath, icon, pathname, title }) => ({
