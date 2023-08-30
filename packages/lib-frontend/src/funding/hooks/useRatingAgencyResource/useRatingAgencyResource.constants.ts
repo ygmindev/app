81236fd46ad76c9f4121732cf2c86d77b6bd0d6f
YG Min <ygminceo@gmail.com>
2023-08-29 20:48:49 -0400

    commit

--- packages/lib-frontend/src/funding/hooks/useRatingAgencyResource/useRatingAgencyResource.constants.ts
+++ packages/lib-frontend/src/funding/hooks/useRatingAgencyResource/useRatingAgencyResource.constants.ts
@@ -1,6 +1,4 @@
 import { type GraphQlQueryParamsFieldsModel } from '#lib-frontend/data/utils/graphQlQuery/graphQlQuery.models';
-import { type RatingAgencyModel } from '#lib-shared/{{module}}(camelCase)/resources/RatingAgency/RatingAgency.models';
+import { type RatingAgencyModel } from '#lib-shared/funding/resources/RatingAgency/RatingAgency.models';
 
-export const RatingAgency(CONSTANTCASE)_FIELDS: GraphQlQueryParamsFieldsModel<RatingAgencyModel> = [
-  '_id',
-];
+export const RATING_AGENCY_FIELDS: GraphQlQueryParamsFieldsModel<RatingAgencyModel> = ['_id'];
--- packages/tool-generate/templates/js-entity-resource/lib-frontend/src/{{MODULE}}(camelCase)/hooks/use{{NAME}}(pascalCase)Resource/use{{NAME}}(pascalCase)Resource.constants.ts
+++ packages/tool-generate/templates/js-entity-resource/lib-frontend/src/{{MODULE}}(camelCase)/hooks/use{{NAME}}(pascalCase)Resource/use{{NAME}}(pascalCase)Resource.constants.ts
@@ -1,6 +1,6 @@
 import { type GraphQlQueryParamsFieldsModel } from '#lib-frontend/data/utils/graphQlQuery/graphQlQuery.models';
 import { type {{NAME}}(pascalCase)Model } from '#lib-shared/{{MODULE}}(camelCase)/resources/{{NAME}}(pascalCase)/{{NAME}}(pascalCase).models';
 
-export const {{NAME}}(CONSTANTCASE)_FIELDS: GraphQlQueryParamsFieldsModel<{{NAME}}(pascalCase)Model> = [
+export const {{NAME}}(constantCase)_FIELDS: GraphQlQueryParamsFieldsModel<{{NAME}}(pascalCase)Model> = [
   '_id',
 ];
