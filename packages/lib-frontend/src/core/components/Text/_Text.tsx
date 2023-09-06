9fb9f229211fcf0cf69c00963a094b16f4988a7b
YG Min <ygminceo@gmail.com>
2023-09-06 03:00:43 -0400

    commit

--- packages/lib-frontend/src/core/components/Text/_Text.tsx
+++ packages/lib-frontend/src/core/components/Text/_Text.tsx
@@ -1,3 +1,4 @@
+import isArray from 'lodash/isArray';
 import lowerCase from 'lodash/lowerCase';
 import upperCase from 'lodash/upperCase';
 import { type TextProps } from 'react-native';
@@ -5,9 +6,26 @@ import { Text } from 'react-native';
 
 import { type _TextPropsModel } from '#lib-frontend/core/components/Text/_Text.models';
 import { TEXT_CASING } from '#lib-frontend/core/components/Text/Text.constants';
+import { type TextCasingModel } from '#lib-frontend/core/components/Text/Text.models';
 import { composeComponent } from '#lib-frontend/core/utils/composeComponent/composeComponent';
 import { type ComposeComponentParamsModel } from '#lib-frontend/core/utils/composeComponent/composeComponent.models';
 import { type TextStyleModel } from '#lib-frontend/style/style.models';
+
+const capitalize = (text: string, casing?: TextCasingModel): string => {
+  switch (casing) {
+    case TEXT_CASING.CAPITALIZE: {
+      return `${text[0].toUpperCase()}${text.substring(1)}`;
+    }
+    case TEXT_CASING.UPPERCASE: {
+      return upperCase(text);
+    }
+    case TEXT_CASING.LOWERCASE: {
+      return lowerCase(text);
+    }
+    default:
+      return text;
+  }
+};
 
 export const _textParams: ComposeComponentParamsModel<_TextPropsModel, TextProps, TextStyleModel> =
   {
@@ -19,25 +37,13 @@ export const _textParams: ComposeComponentParamsModel<_TextPropsModel, TextProps
       isEllipsis,
       onPress,
     }: _TextPropsModel): TextProps => {
-      let childrenF = children;
-      if (childrenF) {
-        switch (casing) {
-          case TEXT_CASING.CAPITALIZE: {
-            childrenF = `${childrenF[0].toUpperCase()}${childrenF.substring(1)}`;
-            break;
-          }
-          case TEXT_CASING.UPPERCASE: {
-            childrenF = upperCase(childrenF);
-            break;
-          }
-          case TEXT_CASING.LOWERCASE: {
-            childrenF = lowerCase(childrenF);
-            break;
-          }
-        }
-      }
+      isArray();
       return {
-        children: childrenF,
+        children: isArray(children)
+          ? children
+          : children
+          ? capitalize(children, casing)
+          : undefined,
         ellipsizeMode: isEllipsis ? 'tail' : undefined,
         numberOfLines: isEllipsis ? 1 : undefined,
         onPress,
