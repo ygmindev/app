1df96279e743f7bf1f2d1270ba0316663e4935f4
YG Min <ygminceo@gmail.com>
2023-09-09 04:04:33 -0400

    commit

---
+++ packages/lib-frontend/src/app/components/FloatingFooter/FloatingFooter.tsx
@@ -0,0 +1,21 @@
+import { type FloatingFooterPropsModel } from '#lib-frontend/app/components/FloatingFooter/FloatingFooter.models';
+import { Portal } from '#lib-frontend/core/components/Portal/Portal';
+import { Wrapper } from '#lib-frontend/core/components/Wrapper/Wrapper';
+import { type FCModel } from '#lib-frontend/core/core.models';
+import { useTheme } from '#lib-frontend/style/hooks/useTheme/useTheme';
+import { THEME_SIZE } from '#lib-frontend/style/style.constants';
+import { SHAPE_POSITION } from '#lib-frontend/style/utils/styler/shapeStyler/shapeStyler.constants';
+
+export const FloatingFooter: FCModel<FloatingFooterPropsModel> = ({ children }) => {
+  const theme = useTheme();
+  return (
+    <Portal>
+      <Wrapper
+        bottom={theme.shape.spacing[THEME_SIZE.MEDIUM]}
+        isFullWidth
+        position={SHAPE_POSITION.ABSOLUTE}>
+        <Wrapper mHorizontal="auto">{children}</Wrapper>
+      </Wrapper>
+    </Portal>
+  );
+};
--- packages/lib-frontend/src/funding/containers/CreditRatingForm/CreditRatingForm.tsx
+++ packages/lib-frontend/src/funding/containers/CreditRatingForm/CreditRatingForm.tsx
@@ -1,59 +1,92 @@
 import { useRef, useState } from 'react';
 
+import { FloatingFooter } from '#lib-frontend/app/components/FloatingFooter/FloatingFooter';
 import { type ModalRefModel } from '#lib-frontend/core/components/Modal/Modal.models';
 import { ModalButton } from '#lib-frontend/core/components/ModalButton/ModalButton';
 import { Text } from '#lib-frontend/core/components/Text/Text';
 import { Tile } from '#lib-frontend/core/components/Tile/Tile';
+import { Wrapper } from '#lib-frontend/core/components/Wrapper/Wrapper';
 import { type LFCModel } from '#lib-frontend/core/core.models';
-import { MainLayout } from '#lib-frontend/core/layouts/MainLayout/MainLayout';
+import { useAsync } from '#lib-frontend/core/hooks/useAsync/useAsync';
+import { FormContainer } from '#lib-frontend/data/components/FormContainer/FormContainer';
 import { Table } from '#lib-frontend/data/components/Table/Table';
 import { type CreditRatingFormPropsModel } from '#lib-frontend/funding/containers/CreditRatingForm/CreditRatingForm.models';
 import { CreditRatingItemForm } from '#lib-frontend/funding/containers/CreditRatingItemForm/CreditRatingItemForm';
 import { useTranslation } from '#lib-frontend/locale/hooks/useTranslation/useTranslation';
 import { useLayoutStyles } from '#lib-frontend/style/hooks/useLayoutStyles/useLayoutStyles';
 import { FONT_TYPE } from '#lib-frontend/style/utils/styler/fontStyler/fontStyler.constants';
+import { sleep } from '#lib-shared/core/utils/sleep/sleep';
 import { withId } from '#lib-shared/core/utils/withId/withId';
-import { type WithIdModel } from '#lib-shared/core/utils/withId/withId.models';
 import { type CreditRatingModel } from '#lib-shared/funding/resources/CreditRating/CreditRating.models';
 
-export const CreditRatingForm: LFCModel<CreditRatingFormPropsModel> = ({ data, ...props }) => {
+export const CreditRatingForm: LFCModel<CreditRatingFormPropsModel> = ({
+  onComplete,
+  onError,
+  onSubmit,
+  onSuccess,
+  ...props
+}) => {
   const { t } = useTranslation();
   const { wrapperProps } = useLayoutStyles({ props });
-  const [values, valuesSet] = useState<Array<WithIdModel & CreditRatingModel>>([]);
-  const ref = useRef<ModalRefModel>(null);
+  const [values, valuesSet] = useState<Array<CreditRatingModel>>([]);
+  const modalRef = useRef<ModalRefModel>(null);
 
-  const handleSubmit = async (value: CreditRatingModel): Promise<void> => {
+  const handleAdd = async (value: CreditRatingModel): Promise<void> => {
     valuesSet(withId([...values, value]));
-    ref.current?.toggle(false);
+    modalRef.current?.toggle(false);
   };
 
-  return (
-    <MainLayout
-      {...wrapperProps}
-      s>
-      {values.length ? (
-        values.map(({ _agency, id, longTermCategory, longTermWatch }) => (
-          <Tile
-            key={id}
-            title={_agency}>
-            <Table
-              columns={[{ id: 'name' }, { id: 'value' }]}
-              data={[
-                { name: 'longTermCategory', value: longTermCategory },
-                { name: 'longTermWatch', value: longTermWatch },
-              ]}
-            />
-          </Tile>
-        ))
-      ) : (
-        <Text type={FONT_TYPE.HEADLINE}>{t('funding:noCreditRatingMessage')}</Text>
-      )}
+  const handleSubmit = async (): Promise<void> => {
+    // onSubmit && onSubmit({
+    //   [CREDIT_RATING_RESOURCE_NAME]?: values.map((value) => ({
+    //   }))
+    //  });
+  };
 
-      <ModalButton
-        element={<CreditRatingItemForm onSubmit={handleSubmit} />}
-        ref={ref}>
-        {t('core:add', { value: t('funding:creditRating') })}
-      </ModalButton>
-    </MainLayout>
+  useAsync(async () => {
+    await sleep();
+    !values.length && modalRef.current?.toggle(true);
+  }, []);
+
+  return (
+    <>
+      <FormContainer
+        {...wrapperProps}
+        onComplete={onComplete}
+        onError={onError}
+        onSubmit={handleSubmit}
+        onSuccess={onSuccess}
+        topElement={() =>
+          values.length ? (
+            <Wrapper s>
+              {values.map(({ _agency, id, longTermCategory, longTermWatch }) => (
+                <Tile
+                  key={id}
+                  title={_agency}>
+                  <Table
+                    columns={[{ id: 'name' }, { id: 'value' }]}
+                    data={[
+                      { name: 'longTermCategory', value: longTermCategory },
+                      { name: 'longTermWatch', value: longTermWatch },
+                    ]}
+                  />
+                </Tile>
+              ))}
+            </Wrapper>
+          ) : (
+            <Text type={FONT_TYPE.HEADLINE}>{t('funding:noCreditRatingMessage')}</Text>
+          )
+        }
+      />
+
+      <FloatingFooter>
+        <ModalButton
+          element={<CreditRatingItemForm onSubmit={handleAdd} />}
+          icon="add"
+          ref={modalRef}>
+          {t('core:add', { value: t('funding:creditRating') })}
+        </ModalButton>
+      </FloatingFooter>
+    </>
   );
 };
