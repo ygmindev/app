fcc74fd426c12b50b5d97bd0f81d0a1f12987222
YG Min <ygminceo@gmail.com>
2023-10-14 13:32:44 -0400

    commit

--- packages/lib-shared/src/user/resources/User/User.models.ts
+++ packages/lib-shared/src/user/resources/User/User.models.ts
@@ -1,21 +1,24 @@
-import { type ACCESS_RESOURCE_NAME } from '#lib-shared/auth/resources/Access/Access.constants';
-import { type AccessModel } from '#lib-shared/auth/resources/Access/Access.models';
+import { type BANK_RESOURCE_NAME } from '#lib-shared/billing/resources/Bank/Bank.constants';
+import { type BankModel } from '#lib-shared/billing/resources/Bank/Bank.models';
+import { type CARD_RESOURCE_NAME } from '#lib-shared/billing/resources/Card/Card.constants';
+import { type CardModel } from '#lib-shared/billing/resources/Card/Card.models';
+import { type PAYMENT_METHOD_RESOURCE_NAME } from '#lib-shared/billing/resources/PaymentMethod/PaymentMethod.constants';
+import { type PaymentMethodModel } from '#lib-shared/billing/resources/PaymentMethod/PaymentMethod.models';
 import {
   type EntityResourceDataModel,
   type EntityResourceModel,
 } from '#lib-shared/resource/resources/EntityResource/EntityResource.models';
-import { type CollectionModel } from '#lib-shared/resource/utils/Collection/Collection.models';
+import { type LINKED_USER_RESOURCE_NAME } from '#lib-shared/user/resources/LinkedUser/LinkedUser.constants';
+import { type LinkedUserModel } from '#lib-shared/user/resources/LinkedUser/LinkedUser.models';
 
 export type UserModel = EntityResourceModel & {
-  // [BANK_RESOURCE_NAME]?: Array<BankModel>;
+  [BANK_RESOURCE_NAME]?: Array<BankModel>;
 
-  // [CARD_RESOURCE_NAME]?: Array<CardModel>;
+  [CARD_RESOURCE_NAME]?: Array<CardModel>;
 
-  // [LINKED_USER_RESOURCE_NAME]?: Array<LinkedUserModel>;
+  [LINKED_USER_RESOURCE_NAME]?: Array<LinkedUserModel>;
 
-  // [PAYMENT_METHOD_RESOURCE_NAME]?: Array<PaymentMethodModel>;
-
-  [ACCESS_RESOURCE_NAME]?: CollectionModel<AccessModel>;
+  [PAYMENT_METHOD_RESOURCE_NAME]?: Array<PaymentMethodModel>;
 
   callingCode?: string;
 
