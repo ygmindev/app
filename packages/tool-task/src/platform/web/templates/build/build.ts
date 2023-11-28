3cec7ac82d917b27a8ed371da5d8e9e32c92b6e0
YG Min <ygmindev@gmail.com>
2023-11-28 14:41:24 -0500

    commit

--- packages/tool-task/src/platform/web/templates/build/build.ts
+++ packages/tool-task/src/platform/web/templates/build/build.ts
@@ -1,8 +1,10 @@
 import { fromExecutable } from '#lib-backend/file/utils/fromExecutable/fromExecutable';
 import { joinPaths } from '#lib-backend/file/utils/joinPaths/joinPaths';
+import { config as fileConfig } from '#lib-config/core/file/file';
 import { config } from '#lib-config/platform/web/web';
 import { ENVIRONMENT } from '#lib-shared/environment/environment.constants';
 import { type TaskParamsModel } from '#tool-task/core/core.models';
+import { runServer } from '#tool-task/platform/server/utils/runServer/runServer';
 
 export const build: TaskParamsModel<unknown> = {
   environment: ENVIRONMENT.PRODUCTION,
@@ -14,6 +16,8 @@ export const build: TaskParamsModel<unknown> = {
   task: [
     ({ root }) => fromExecutable(`vite build --config ${joinPaths([root, config().configFile])}`),
 
-    // ({ root }) => runServer({ pathname: joinPaths([root, fileConfig.distPath, 'client']) }),
+    ({ root }) =>
+      process.env.APP_IS_STATIC_SERVER &&
+      runServer({ pathname: joinPaths([root, fileConfig.distPath, 'client']) }),
   ],
 };
