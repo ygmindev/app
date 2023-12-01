78718d5dcf533af6294535de602c61534f25e25f
YG Min <ygmindev@gmail.com>
2023-11-30 19:10:23 -0500

    commit

--- packages/app-web-admin/src/routes.tsx
+++ packages/app-web-admin/src/routes.tsx
@@ -1,7 +1,10 @@
+import { DEPENDENCY } from '#lib-frontend/admin/admin.constants';
 import { AdminHomePage } from '#lib-frontend/admin/pages/AdminHomePage/AdminHomePage';
+import { DependenciesPage } from '#lib-frontend/admin/pages/DependenciesPage/DependenciesPage';
 import { ResourcePage } from '#lib-frontend/resource/pages/ResourcePage/ResourcePage';
 import { RESOURCE } from '#lib-frontend/resource/resource.constants';
 import { type RouteModel } from '#lib-frontend/route/route.models';
+import { getRouteGroup } from '#lib-frontend/route/utils/getRouteGroup/getRouteGroup';
 import { getRoutes } from '#lib-frontend/route/utils/getRoutes/getRoutes';
 
 export const routes: Array<RouteModel> = getRoutes({
@@ -16,5 +19,10 @@ export const routes: Array<RouteModel> = getRoutes({
       isProtectable: true,
       pathname: `${RESOURCE}/:resourceid?`,
     },
+
+    getRouteGroup({
+      element: <DependenciesPage />,
+      pathname: DEPENDENCY,
+    }),
   ],
 });
