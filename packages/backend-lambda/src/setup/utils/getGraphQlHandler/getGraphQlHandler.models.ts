064071757c984f5e0059e5f9987bfbc1f3901727
YG Min <ygmindev@gmail.com>
2024-01-02 16:47:35 -0500

    commit

--- packages/backend-lambda/src/setup/utils/getGraphQlHandler/getGraphQlHandler.models.ts
+++
@@ -1,5 +0,0 @@
-import { type Handler } from 'aws-lambda';
-
-import { type InitializeModel as BackendInitializeModel } from '#lib-backend/setup/utils/initialize/initialize.models';
-
-export type GetGraphQlHandlerModel = BackendInitializeModel & { handler: Handler };
