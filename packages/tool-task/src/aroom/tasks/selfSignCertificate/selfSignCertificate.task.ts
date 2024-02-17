78617428fd7a443ffafe2568395b0064896dacea
YG Min <ygmindev@gmail.com>
2024-02-17 17:20:40 -0500

    commit

--- packages/tool-task/src/aroom/tasks/selfSignCertificate/selfSignCertificate.task.ts
+++ packages/tool-task/src/aroom/tasks/selfSignCertificate/selfSignCertificate.task.ts
@@ -1,20 +1,20 @@
+import { joinPaths } from '@lib/backend/file/utils/joinPaths/joinPaths';
 import { config as httpConfig } from '@lib/config/http/http/http';
+import { ENVIRONMENT } from '@lib/shared/environment/environment.constants';
 import { type TaskParamsModel } from '@tool/task/core/core.models';
 
 const selfSignCertificate: TaskParamsModel<unknown> = {
+  environment: ENVIRONMENT.DEVELOPMENT,
+
   name: 'self-sign-certificate',
 
   task: [
     () => {
       const httpConfigF = httpConfig();
-      const { privateKeyFile, publicKeyFile } = httpConfigF.certificate;
-      return `mkcert -install -cert-file ${publicKeyFile} -key-file ${privateKeyFile}`;
+      const { certificateDir, privateKeyFile, publicKeyFile } = httpConfigF.certificate;
+      return `CAROOT=${certificateDir} mkcert -install -cert-file ${joinPaths([certificateDir, publicKeyFile])} -key-file ${joinPaths([certificateDir, privateKeyFile])} localhost`;
     },
   ],
-
-  variables: () => ({
-    CAROOT: httpConfig().certificate.certificateDir,
-  }),
 };
 
 export default selfSignCertificate;
