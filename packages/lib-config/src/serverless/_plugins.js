2fb7bf26204310c66f5ba555f96743825bcdafed
YG Min <ygmindev@gmail.com>
2024-05-28 17:50:46 -0400

    commit

--- packages/lib-config/src/serverless/_plugins.js
+++ packages/lib-config/src/serverless/_plugins.js
@@ -1,6 +1,6 @@
 import { nodeRegister } from '../../../lib-shared/src/core/utils/nodeRegister/nodeRegister';
-import { _plugins } from '../node/bundle/_plugins';
-import bundleConfig from '../node/bundle/bundle.base';
-
 nodeRegister();
+
+import { _plugins } from '@lib/config/node/bundle/_plugins';
+import bundleConfig from '@lib/config/node/bundle/bundle.base';
 module.exports = _plugins({ rootDirs: bundleConfig.params().rootDirs });
