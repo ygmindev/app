839dc7c13b36b242155c897b5a3647fb12300a14
YG Min <ygmindev@gmail.com>
2024-08-15 20:21:50 -0400

    commit

--- packages/app-web/src/ete/billing/billing.ete.ts
+++ packages/app-web/src/ete/billing/billing.ete.ts
@@ -1,11 +1,8 @@
-import { signIn } from '@app/web/ete/auth/utils/signIn/signIn';
 import { TestScreen } from '@lib/backend/test/utils/TestScreen/TestScreen';
 import { APP_URI } from '@lib/shared/http/http.constants';
 
-describe('sign in', () => {
+describe('billing', () => {
   test('works', async () => {
     const screen = new TestScreen({ rootUri: APP_URI });
-    await signIn({ isSnapshot: true, screen });
-    expect(true).toBe(true);
   });
 });
