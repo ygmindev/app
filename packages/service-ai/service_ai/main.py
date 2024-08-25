bec2b678765efb213e6e8b6f8e705da338233bed
YG Min <ygmindev@gmail.com>
2024-08-25 18:45:51 -0400

    commit

--- packages/service-ai/service_ai/main.py
+++ packages/service-ai/service_ai/main.py
@@ -1,4 +1,16 @@
 from lib_python.core.utils.test import test
+from torch import bfloat16
+from transformers import pipeline
+
+model_id = "meta-llama/Meta-Llama-3-8B"
+
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": bfloat16},
+    device_map="auto",
+)
+pipe("Hey how are you doing today?")
 
 print("hi")
 test()
