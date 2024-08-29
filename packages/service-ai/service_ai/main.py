75d17f481e819cc785dd64bf77cc991f63a75cc6
YG Min <ygmindev@gmail.com>
2024-08-28 21:37:12 -0400

    commit

--- packages/service-ai/service_ai/main.py
+++ packages/service-ai/service_ai/main.py
@@ -1,31 +1,6 @@
-from lib_python.core.utils.test import test
-from torch import bfloat16
-from transformers import pipeline
+import torch
 
-test()
-
-# model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-
-# pipe = pipeline(
-#     "text-generation",
-#     model=model_id,
-#     model_kwargs={"torch_dtype": bfloat16},
-#     # device_map="auto",
-# )
-
-# messages = [
-#     {
-#         "role": "system",
-#         "content": "You are a pirate chatbot who always responds in pirate speak!",
-#     },
-#     {
-#         "role": "user",
-#         "content": "Who are you?",
-#     },
-# ]
-
-# outputs = pipeline(
-#     messages,
-#     max_new_tokens=256,
-# )
-# print(outputs[0]["generated_text"][-1])
+device = (
+    "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+)
+print(f"Using {device} device")
