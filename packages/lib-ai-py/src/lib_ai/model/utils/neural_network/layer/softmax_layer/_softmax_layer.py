e434619e66788c7b431705f1472dc48f69187568
YG Min <ygmindev@gmail.com>
2024-11-11 23:22:12 -0500

    commit

--- packages/lib-ai-py/src/lib_ai/model/utils/neural_network/layer/softmax_layer/_softmax_layer.py
+++ packages/lib-ai-py/src/lib_ai/model/utils/neural_network/layer/softmax_layer/_softmax_layer.py
@@ -1,11 +1,11 @@
 from lib_ai.model.utils.neural_network.layer.softmax_layer._softmax_layer_models import (
     _SoftmaxLayerModel,
 )
-from torch.nn import LogSoftmax
+from torch.nn import Softmax
 
 
 class _SoftmaxLayer(_SoftmaxLayerModel):
     def __init__(
         self,
     ) -> None:
-        self._layer = LogSoftmax(dim=1)
+        self._layer = Softmax(dim=1)
