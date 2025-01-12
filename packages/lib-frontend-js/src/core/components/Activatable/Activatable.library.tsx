063298465d735a4ec54a87438a0312ccc40fe1be
YG Min <ygmindev@gmail.com>
2025-01-12 00:10:00 -0500

    commit

--- packages/lib-frontend-js/src/core/components/Activatable/Activatable.library.tsx
+++ packages/lib-frontend-js/src/core/components/Activatable/Activatable.library.tsx
@@ -1,24 +1,20 @@
 import { Activatable } from '@lib/frontend/core/components/Activatable/Activatable';
 import { ACTIVATABLE_TRIGGER } from '@lib/frontend/core/components/Activatable/Activatable.constants';
 import { type ActivatablePropsModel } from '@lib/frontend/core/components/Activatable/Activatable.models';
+import { Text } from '@lib/frontend/core/components/Text/Text';
+import { Wrapper } from '@lib/frontend/core/components/Wrapper/Wrapper';
 import { type LibraryPropsModel } from '@lib/frontend/library/components/Library/Library.models';
 import { cartesianObject } from '@lib/shared/core/utils/cartesianObject/cartesianObject';
-import { cloneElement, useState } from 'react';
 
 export const props: LibraryPropsModel<ActivatablePropsModel> = {
   Component: Activatable,
-  Renderer: ({ element }) => {
-    const [isActive, isActiveSet] = useState<boolean>(false);
-    return (
-      <Activatable
-        {...props}
-        onActive={() => isActiveSet(true)}
-        onInactive={() => isActiveSet(false)}>
-        {cloneElement(element, { children: `isActive: ${isActive}` })}
-      </Activatable>
-    );
+  defaultProps: {
+    children: (isActive) => (
+      <Wrapper>
+        <Text>{`${isActive}`}</Text>
+      </Wrapper>
+    ),
   },
-  defaultProps: {},
   variants: cartesianObject({ trigger: Object.values(ACTIVATABLE_TRIGGER) }).map((props) => ({
     props,
   })),
