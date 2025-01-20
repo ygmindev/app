a275c48c3d1fcb5e912dd893a4c3f36d71c59e6a
YG Min <ygmindev@gmail.com>
2025-01-19 21:08:24 -0500

    commit

--- packages/lib-frontend-js/src/data/providers/QueryProvider/_QueryProvider.tsx
+++ packages/lib-frontend-js/src/data/providers/QueryProvider/_QueryProvider.tsx
@@ -2,7 +2,6 @@ import { type FCModel } from '@lib/frontend/core/core.models';
 import { type _QueryProviderPropsModel } from '@lib/frontend/data/providers/QueryProvider/_QueryProvider.models';
 import { QueryClient } from '@lib/frontend/data/utils/QueryClient/QueryClient';
 import { HydrationBoundary, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
 export const _QueryProvider: FCModel<_QueryProviderPropsModel> = ({ children, value }) => {
@@ -10,8 +9,6 @@ export const _QueryProvider: FCModel<_QueryProviderPropsModel> = ({ children, va
   return (
     <QueryClientProvider client={client}>
       <HydrationBoundary state={value?.state}>{children}</HydrationBoundary>
-
-      {process.env.APP_IS_DEBUG === 'true' && <ReactQueryDevtools />}
     </QueryClientProvider>
   );
 };
