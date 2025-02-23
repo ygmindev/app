70f10a85174e999c1d4be119edf4ebd9432232db
YG Min <ygmindev@gmail.com>
2025-02-22 19:42:10 -0500

    commit

--- packages/tool-task-js/templates/entity-resource-js/lib-frontend/src/{{MODULE}}(camelCase)/resources/{{NAME}}(pascalCase)/{{NAME}}(pascalCase).constants.ts
+++
@@ -1,9 +0,0 @@
-import { type ResourceParamsModel } from '@lib/frontend/resource/resource.models';
-import { UTILITY_RESOURCE_NAME } from '@lib/shared/{{MODULE}}(camelCase)/resources/Utility/Utility.constants';
-import { {{NAME}}(constantCase)_RESOURCE_NAME } from '@lib/shared/{{MODULE}}(camelCase)/resources/{{NAME}}(pascalCase)/{{NAME}}(pascalCase).constants';
-import { type {{NAME}}(pascalCase)Model } from '@lib/shared/{{MODULE}}(camelCase)/resources/{{NAME}}(pascalCase)/{{NAME}}(pascalCase).models';
-
-export const {{NAME}}(constantCase)_RESOURCE_PARAMS = {
-  fields: [],
-  name: {{NAME}}(constantCase)_RESOURCE_NAME,
-} satisfies ResourceParamsModel<{{NAME}}(pascalCase)Model>;
--- packages/tool-task-js/templates/entity-resource-js/lib-shared/src/{{MODULE}}(camelCase)/resources/{{NAME}}(pascalCase)/{{NAME}}(pascalCase).constants.ts
+++
@@ -1,1 +0,0 @@
-export const {{NAME}}(constantCase)_RESOURCE_NAME = '{{NAME}}(pascalCase)';
