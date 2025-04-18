7bafa8338339b0ad8bc34f5057f32568f371f2dc
YG Min <ygmindev@gmail.com>
2025-04-18 16:02:54 -0400

    commit

--- packages/lib-shared-js/src/core/utils/Container/_Container.ts
+++ packages/lib-shared-js/src/core/utils/Container/_Container.ts
@@ -12,7 +12,7 @@ export const _Container: _ContainerModel = {
   container: () => _container,
 
   get: <TType extends unknown>(type: ClassModel<TType> | string, name?: string): TType =>
-    _container.get<TType>(name ?? type),
+    _container.get<TType>(type, name ? { name } : undefined),
 
   set: <TType extends unknown>(
     type: ClassModel<TType> | string,
@@ -25,3 +25,31 @@ export const _Container: _ContainerModel = {
     name && valueF.whenNamed(name);
   },
 };
+
+// import { type ClassModel } from '@lib/shared/core/core.models';
+// import { type _ContainerModel } from '@lib/shared/core/utils/Container/_Container.models';
+// import { Container } from 'inversify';
+// import isFunction from 'lodash/isFunction';
+
+// export const _container = new Container({
+//   autobind: true,
+//   defaultScope: 'Singleton',
+// });
+
+// export const _Container: _ContainerModel = {
+//   container: () => _container,
+
+//   get: <TType extends unknown>(type: ClassModel<TType> | string, name?: string): TType =>
+//     _container.get<TType>(name ?? type),
+
+//   set: <TType extends unknown>(
+//     type: ClassModel<TType> | string,
+//     value: TType | ClassModel<TType>,
+//     name?: string,
+//   ): void => {
+//     const valueF = isFunction(value)
+//       ? _container.bind<TType>(type).to(value as ClassModel<TType>)
+//       : _container.bind<TType>(type).toDynamicValue(() => value as TType);
+//     name && valueF.whenNamed(name);
+//   },
+// };
