de19c494a766eefc046a193520b0cdca37a64c42
YG Min <ygmindev@gmail.com>
2025-05-26 11:13:09 -0400

    commit

--- packages/lib-backend-js/src/file/utils/fromExecutable/fromExecutable.ts
+++ packages/lib-backend-js/src/file/utils/fromExecutable/fromExecutable.ts
@@ -3,6 +3,7 @@ import {
   type FromExecutableParamsModel,
 } from '@lib/backend/file/utils/fromExecutable/fromExecutable.models';
 import { fromModules } from '@lib/backend/file/utils/fromModules/fromModules';
+import { joinPaths } from '@lib/backend/file/utils/joinPaths/joinPaths';
 import { NotFoundError } from '@lib/shared/core/errors/NotFoundError/NotFoundError';
 import { filterNil } from '@lib/shared/core/utils/filterNil/filterNil';
 import { execSync } from 'child_process';
@@ -17,7 +18,7 @@ const resolveCommand = (value: string): string | null => {
 };
 
 export const fromExecutable = (...paths: FromExecutableParamsModel): FromExecutableModel => {
-  const [command, ...rest] = filterNil(paths);
+  const [command, ...rest] = joinPaths(filterNil(paths)).split(' ');
   const options: Array<() => string> = [
     () => execSync(`zsh -l -c "which ${command}"`).toString().trim(),
     () => fromModules('.bin', command),
