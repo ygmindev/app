ab5c55f34ba6c6c71503bad2a2339274c6c90090
YG Min <ygmindev@gmail.com>
2025-06-05 20:30:40 -0400

    commit

--- packages/app-web-library-js/tasks.ts
+++ packages/app-web-library-js/tasks.ts
@@ -1,12 +1,5 @@
-import { type TaskParamsModel } from '@tool/task/core/core.models';
-import { nodeTasks } from '@tool/task/node/utils/nodeTasks/nodeTasks';
-import { build } from '@tool/task/web/templates/build/build';
-import { dev } from '@tool/task/web/templates/dev/dev';
+import { webTasks } from '@tool/task/web/utils/webTasks/webTasks';
 
-const tasks = nodeTasks({
-  additionalTasks: [dev, build],
-
-  eteTasks: ['run bsd', 'run awld'],
-}) satisfies Array<TaskParamsModel<unknown>>;
+const tasks = webTasks();
 
 export default tasks;
--- packages/lib-config-js/src/routes/routes.library.tsx
+++ packages/lib-config-js/src/routes/routes.library.tsx
@@ -3,7 +3,7 @@ import { defineConfig } from '@lib/config/utils/defineConfig/defineConfig';
 import { getComponentDisplayName } from '@lib/frontend/core/utils/getComponentDisplayName/getComponentDisplayName';
 import { Library } from '@lib/frontend/library/components/Library/Library';
 import { type LibraryPropsModel } from '@lib/frontend/library/components/Library/Library.models';
-import { LIBRARY_PROPS } from '@lib/frontend/library/library.constants';
+import { LIBRARY, LIBRARY_PROPS } from '@lib/frontend/library/library.constants';
 import { ROUTE_NAVIGATION } from '@lib/frontend/route/route.constants';
 import { getRoutes } from '@lib/frontend/route/utils/getRoutes/getRoutes';
 import { trimPathname } from '@lib/frontend/route/utils/trimPathname/trimPathname';
@@ -14,7 +14,8 @@ export const config = defineConfig<RoutesConfigModel>({
     routes: getRoutes([
       {
         navigation: ROUTE_NAVIGATION.NAVIGATION,
-        pathname: '/',
+        pathname: LIBRARY,
+        prerender: true,
         routes: LIBRARY_PROPS.map((props) => {
           const id = (props.title =
             props.title ?? getComponentDisplayName(props.Component as ComponentType));
@@ -22,6 +23,7 @@ export const config = defineConfig<RoutesConfigModel>({
             category: props.category,
             element: <Library<unknown> {...(props as LibraryPropsModel<unknown>)} />,
             pathname: trimPathname(id),
+            prerender: true,
             title: id,
           };
         }),
