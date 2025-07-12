4038b13639e6e5e1fdf039c34e47c074df7f0c79
YG Min <ygmindev@gmail.com>
2025-07-12 09:12:09 -0400

    commit

--- packages/lib-frontend-js/src/map/hooks/useMapQuery/useMapQuery.tsx
+++ packages/lib-frontend-js/src/map/hooks/useMapQuery/useMapQuery.tsx
@@ -1,13 +1,12 @@
+import { useDebounce } from '@lib/frontend/core/utils/useDebounce/useDebounce';
 import { useMutation } from '@lib/frontend/data/hooks/useMutation/useMutation';
 import { _useMapQuery } from '@lib/frontend/map/hooks/useMapQuery/_useMapQuery';
 import { USE_MAP_QUERY_DEBOUNCE_DURATION } from '@lib/frontend/map/hooks/useMapQuery/useMapQuery.constants';
 import { type UseMapQueryModel } from '@lib/frontend/map/hooks/useMapQuery/useMapQuery.models';
-import { debounce } from '@lib/shared/core/utils/debounce/debounce';
-import { useCallback } from 'react';
 
 export const useMapQuery = (): UseMapQueryModel => {
   const { data, mutate: _mutate } = _useMapQuery();
   const { isLoading, mutate } = useMutation('map', _mutate);
-  const queryF = useCallback(debounce(mutate, { duration: USE_MAP_QUERY_DEBOUNCE_DURATION }), []);
+  const queryF = useDebounce(mutate, { duration: USE_MAP_QUERY_DEBOUNCE_DURATION });
   return { data, isLoading, mutate: queryF };
 };
