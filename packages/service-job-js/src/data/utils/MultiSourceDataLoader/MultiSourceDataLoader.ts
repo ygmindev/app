f1b1547c0a3d3eb6fe44ab1d666137bd05449dcd
YG Min <ygmindev@gmail.com>
2025-08-12 22:56:24 -0400

    commit

--- packages/service-job-js/src/data/utils/MultiSourceDataLoader/MultiSourceDataLoader.ts
+++ packages/service-job-js/src/data/utils/MultiSourceDataLoader/MultiSourceDataLoader.ts
@@ -1,6 +1,6 @@
+import { type SourcedEntityResourceModel } from '@lib/model/data/SourcedEntityResource/SourcedEntityResource.models';
 import { mapParallel } from '@lib/shared/core/utils/mapParallel/mapParallel';
 import { logger } from '@lib/shared/logging/utils/Logger/Logger';
-import { type SourcedEntityResourceModel } from '@lib/model/data/SourcedEntityResource/SourcedEntityResource.models';
 import { DataLoader } from '@service/job/data/utils/DataLoader/DataLoader';
 import {
   type MultiSourceDataLoaderModel,
