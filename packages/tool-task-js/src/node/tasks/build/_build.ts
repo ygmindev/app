8007bd1494aeb775d0609b02ac8573e4ba39ff6c
YG Min <ygmindev@gmail.com>
2025-11-06 22:15:48 -0500

    commit

--- packages/tool-task-js/src/node/tasks/build/_build.ts
+++ packages/tool-task-js/src/node/tasks/build/_build.ts
@@ -1,9 +1,10 @@
 import { config as bundleConfig } from '@lib/config/node/bundle/bundle.node';
+import { logger } from '@lib/shared/logging/utils/Logger/Logger';
 import {
   type _BuildModel,
   type _BuildParamsModel,
 } from '@tool/task/node/tasks/build/_build.models';
-import { build } from 'esbuild';
+import { build, context } from 'esbuild';
 
 export const _build = async ({
   entryFiles,
@@ -14,5 +15,13 @@ export const _build = async ({
     isTranspileProject: true,
     outputDir,
   });
-  esbuildConfig && (await build(esbuildConfig));
+  if (esbuildConfig) {
+    if (process.env.NODE_ENV === 'development') {
+      logger.progress('build watching');
+      await (await context(esbuildConfig)).watch();
+      await new Promise(() => {});
+    } else {
+      await build(esbuildConfig);
+    }
+  }
 };
