59763298b251eae9efefccfb55e1a38f11e93740
YG Min <ygmindev@gmail.com>
2025-12-08 21:31:21 -0500

    commit

---
+++ packages/tool-task-js/src/dev/tasks/ping/ping.models.ts
@@ -0,0 +1,5 @@
+export type PingParamsModel = {};
+
+export type PingModel = {
+  message: string;
+};
---
+++ packages/tool-task-js/src/dev/tasks/ping/ping.task.ts
@@ -0,0 +1,10 @@
+import { sleep } from '@lib/shared/core/utils/sleep/sleep';
+import { buildTask } from '@tool/task/core/utils/buildTask/buildTask';
+import { type PingModel, type PingParamsModel } from '@tool/task/dev/tasks/ping/ping.models';
+
+export const ping = buildTask({
+  task: async (params: PingParamsModel): Promise<PingModel> => {
+    await sleep(5000);
+    return { message: 'success' };
+  },
+});
