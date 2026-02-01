a34c242c38da9807afa9e225133dd868ead60a56
YG Min <ygmindev@gmail.com>
2026-01-31 23:46:48 -0500

    commit

--- packages/lib-frontend-js/src/app/containers/AppProvider/AppProvider.tsx
+++ packages/lib-frontend-js/src/app/containers/AppProvider/AppProvider.tsx
@@ -9,11 +9,9 @@ import {
   type PortalsStateModel,
 } from '@lib/frontend/app/containers/AppProvider/AppProvider.models';
 import { type FCModel } from '@lib/frontend/core/core.models';
-import { useContainer } from '@lib/frontend/core/hooks/useContainer/useContainer';
 import { display } from '@lib/frontend/core/utils/display/display';
 import { useStore } from '@lib/frontend/state/hooks/useStore/useStore';
 import { debounce } from '@lib/shared/core/utils/debounce/debounce';
-import { PubSub } from '@lib/shared/core/utils/PubSub/PubSub';
 import { updateArray } from '@lib/shared/core/utils/updateArray/updateArray';
 import { createContext, useEffect, useReducer } from 'react';
 
@@ -52,7 +50,7 @@ export const AppProvider: FCModel<AppProviderPropsModel> = ({ children }) => {
   const [, dimensionSet] = useStore('app.dimension');
   const [portals, dispatch] = useReducer(portalsReducer, {});
 
-  const pubSub = useContainer(PubSub);
+  // const pubSub = useContainer(PubSub);
 
   const update = debounce(() => dimensionSet(display.getDimension()), {
     duration: USE_DIMENSION_DELAY,
@@ -63,9 +61,81 @@ export const AppProvider: FCModel<AppProviderPropsModel> = ({ children }) => {
     display.subscribeResize(update);
     return () => {
       display.unsubscribeResize(update);
-      void pubSub.cleanUp();
+      // void pubSub.cleanUp();
     };
   }, []);
 
   return <PortalContext.Provider value={{ dispatch, portals }}>{children}</PortalContext.Provider>;
 };
+
+// import {
+//   PORTAL_ACTION_TYPE,
+//   USE_DIMENSION_DELAY,
+// } from '@lib/frontend/app/containers/AppProvider/AppProvider.constants';
+// import {
+//   type AppProviderPropsModel,
+//   type PortalsActionModel,
+//   type PortalsContextModel,
+//   type PortalsStateModel,
+// } from '@lib/frontend/app/containers/AppProvider/AppProvider.models';
+// import { type FCModel } from '@lib/frontend/core/core.models';
+// import { useContainer } from '@lib/frontend/core/hooks/useContainer/useContainer';
+// import { display } from '@lib/frontend/core/utils/display/display';
+// import { useStore } from '@lib/frontend/state/hooks/useStore/useStore';
+// import { debounce } from '@lib/shared/core/utils/debounce/debounce';
+// import { PubSub } from '@lib/shared/core/utils/PubSub/PubSub';
+// import { updateArray } from '@lib/shared/core/utils/updateArray/updateArray';
+// import { createContext, useEffect, useReducer } from 'react';
+
+// export const portalsReducer = (
+//   state: PortalsStateModel,
+//   action: PortalsActionModel,
+// ): PortalsStateModel => {
+//   const { name, root } = action.payload;
+//   switch (action.type) {
+//     case PORTAL_ACTION_TYPE.UPSERT_PORTAL: {
+//       const list = state[root] ?? [];
+//       const updated = updateArray(
+//         list,
+//         (v) => v.name === name,
+//         () => ({ name, node: action.payload.node }),
+//         { isUpsert: true },
+//       );
+//       return { ...state, [root]: updated };
+//     }
+//     case PORTAL_ACTION_TYPE.REMOVE_PORTAL: {
+//       const list = state[root] ?? [];
+//       const filtered = list.filter((v) => v.name !== name);
+//       return { ...state, [root]: filtered };
+//     }
+//     default:
+//       return state;
+//   }
+// };
+
+// export const PortalContext = createContext<PortalsContextModel>({
+//   dispatch: () => null,
+//   portals: {},
+// });
+
+// export const AppProvider: FCModel<AppProviderPropsModel> = ({ children }) => {
+//   const [, dimensionSet] = useStore('app.dimension');
+//   const [portals, dispatch] = useReducer(portalsReducer, {});
+
+//   const pubSub = useContainer(PubSub);
+
+//   const update = debounce(() => dimensionSet(display.getDimension()), {
+//     duration: USE_DIMENSION_DELAY,
+//   });
+
+//   useEffect(() => {
+//     update();
+//     display.subscribeResize(update);
+//     return () => {
+//       display.unsubscribeResize(update);
+//       void pubSub.cleanUp();
+//     };
+//   }, []);
+
+//   return <PortalContext.Provider value={{ dispatch, portals }}>{children}</PortalContext.Provider>;
+// };
