428817ed2b3e81430b5494a05c26bcb58e00267f
YG Min <ygmindev@gmail.com>
2026-01-31 21:54:13 -0500

    commit

--- packages/app-web-kfn-js/__dist__/server/entry.mjs
+++ packages/app-web-kfn-js/__dist__/server/entry.mjs
@@ -1,11 +1,11 @@
-var e=Object.defineProperty,a=(a,t)=>e(a,"name",{value:t,configurable:!0});import{setGlobalContext_prodBuildEntry as t}from"vike/__internal";import{h as s,k as o,C as r,T as i,e as n,L as l,A as g,g as p,t as d,m as u,R as c,d as f}from"./chunks/chunk-MdWKjDAc.js";import{createInstance as m}from"i18next";import{initReactI18next as h}from"react-i18next";import S from"i18next-fs-backend";import"lodash/trim.js";import"@mikro-orm/core";import"lodash/isPlainObject.js";import"lodash/mergeWith.js";import"lodash/uniq.js";import"lodash/cloneDeep.js";import"app-root-path";import"lodash/trimStart.js";import"path";const v=a((...e)=>s(o,...e),"fromPublic");let P;const b=new r({config:a(({isDebug:e,isPreload:a,language:t,languageDefault:s,languages:o,localePath:r,modules:i})=>{const n={debug:!1,defaultNS:!1,fallbackLng:s,initAsync:!a,interpolation:{escapeValue:!1},lng:t,load:"languageOnly",ns:[],partialBundledLanguages:!a,preload:!!a&&[t??s],react:{bindI18n:"languageChanged loaded",defaultTransParent:"div",useSuspense:a},supportedLngs:o.map(({id:e})=>e)};if(r){const e=`${r}/locales/{{lng}}/{{ns}}.json`;n.backend={addPath:e,loadPath:e}}let l;return l=P?P.cloneInstance({...n,forkResourceStore:!0,initImmediate:!1}):P=m(n),l.isInitialized||(i?.forEach(e=>l=l.use(e)),l.init(n)),l},"_internationalize"),params:a(()=>({isDebug:!1,isPreload:!1,languageDefault:l,languages:n,modules:[h],timezoneDefault:i}),"params")}).extend(()=>({isPreload:!0,localePath:v(g),modules:[S]})),y=a(({getContext:e,languages:a})=>async({pageContexts:t})=>{const s=[];return a.forEach(({id:a})=>t.forEach(t=>s.push(async()=>{const s=await e({context:t.context,lang:a,pathname:t.urlOriginal});return{...t,context:s.context,urlOriginal:s.pathname}}))),{prerenderContext:{pageContexts:await p(s)}}},"_onPrerender"),j=a(e=>{const{languageDefault:t,languages:s}=b.params(),o=b.config();return y({getContext:a(async({context:e,lang:a,pathname:s})=>{await o.changeLanguage(a);const r=a===t;return{context:u([{[f]:{i18n:o,lang:a},[c]:{location:{pathname:d(s)}}},e]),pathname:d(r?s:`/${a}/${s}`)}},"getContext"),languages:s})},"onPrerender")(),T=Object.freeze(Object.defineProperty({__proto__:null,onPrerenderStart:j},Symbol.toStringTag,{value:"Module"})),_={},x={},E={},z=[{pageId:"/src/pages",isErrorPage:void 0,routeFilesystem:{routeString:"/",definedAtLocation:"/src/pages/"},loadVirtualFilePageEntry:a(()=>({moduleId:"virtual:vike:page-entry:server:/src/pages",moduleExportsPromise:import("./entries/src_pages.mjs")}),"loadVirtualFilePageEntry"),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},route:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","route"]},valueSerialized:{type:"js-serialized",value:"/*"}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}}],w={configValuesSerialized:{onPrerenderStart:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+onPrerenderStart.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:T}},baseAssets:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","baseAssets"]},valueSerialized:{type:"js-serialized",value:"https://0.0.0.0"}},favicon:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","favicon"]},valueSerialized:{type:"js-serialized",value:"https://0.0.0.0/assets/favicon/favicon.svg"}}}},D={...Object.assign({})};_[".page"]=D;const O={...Object.assign({})};E[".page"]=O;const k={...Object.assign({})};_[".page.server"]=k;const A={...Object.assign({})};E[".page.server"]=A;const C={...Object.assign({})};x[".page.route"]=C;const U={...Object.assign({})};E[".page.client"]=U;t({virtualFileExportsGlobalEntry:Object.freeze(Object.defineProperty({__proto__:null,neverLoaded:{},pageConfigGlobalSerialized:w,pageConfigsSerialized:z,pageFilesEager:x,pageFilesExportNamesEager:E,pageFilesExportNamesLazy:{},pageFilesLazy:_,pageFilesList:[]},Symbol.toStringTag,{value:"Module"})),assetsManifest:{
+var e=Object.defineProperty,a=(a,t)=>e(a,"name",{value:t,configurable:!0});import{setGlobalContext_prodBuildEntry as t}from"vike/__internal";import{h as o,k as s,C as r,T as n,e as i,L as l,A as d,g,t as p,m as u,R as c,d as f}from"./chunks/chunk-MdWKjDAc.js";import{createInstance as m}from"i18next";import{initReactI18next as h}from"react-i18next";import S from"i18next-fs-backend";import"lodash/trim.js";import"@mikro-orm/core";import"lodash/isPlainObject.js";import"lodash/mergeWith.js";import"lodash/uniq.js";import"lodash/cloneDeep.js";import"app-root-path";import"lodash/trimStart.js";import"path";const b=a((...e)=>o(s,...e),"fromPublic");let v;const P=new r({config:a(({isDebug:e,isPreload:a,language:t,languageDefault:o,languages:s,localePath:r,modules:n})=>{const i={debug:!1,defaultNS:!1,fallbackLng:o,initAsync:!a,interpolation:{escapeValue:!1},lng:t,load:"languageOnly",ns:[],partialBundledLanguages:!a,preload:!!a&&[t??o],react:{bindI18n:"languageChanged loaded",defaultTransParent:"div",useSuspense:a},supportedLngs:s.map(({id:e})=>e)};if(r){const e=`${r}/locales/{{lng}}/{{ns}}.json`;i.backend={addPath:e,loadPath:e}}let l;return l=v?v.cloneInstance({...i,forkResourceStore:!0,initImmediate:!1}):v=m(i),l.isInitialized||(n?.forEach(e=>l=l.use(e)),l.init(i)),l},"_internationalize"),params:a(()=>({isDebug:!1,isPreload:!1,languageDefault:l,languages:i,modules:[h],timezoneDefault:n}),"params")}).extend(()=>({isPreload:!0,localePath:b(d),modules:[S]})),y=a(({getContext:e,languages:a})=>async({pageContexts:t})=>{const o=[];return a.forEach(({id:a})=>t.forEach(t=>o.push(async()=>{const o=await e({context:t.context,lang:a,pathname:t.urlOriginal});return{...t,context:o.context,urlOriginal:o.pathname}}))),{prerenderContext:{pageContexts:await g(o)}}},"_onPrerender"),j=a(e=>{const{languageDefault:t,languages:o}=P.params(),s=P.config();return y({getContext:a(async({context:e,lang:a,pathname:o})=>{await s.changeLanguage(a);const r=a===t;return{context:u([{[f]:{i18n:s,lang:a},[c]:{location:{pathname:p(o)}}},e]),pathname:p(r?o:`/${a}/${o}`)}},"getContext"),languages:o})},"onPrerender")(),T=Object.freeze(Object.defineProperty({__proto__:null,onPrerenderStart:j},Symbol.toStringTag,{value:"Module"})),_={},x={},E={},z=[{pageId:"/src/pages",isErrorPage:void 0,routeFilesystem:{routeString:"/",definedAtLocation:"/src/pages/"},loadVirtualFilePageEntry:a(()=>({moduleId:"virtual:vike:page-entry:server:/src/pages",moduleExportsPromise:import("./entries/src_pages.mjs")}),"loadVirtualFilePageEntry"),configValuesSerialized:{isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},route:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","route"]},valueSerialized:{type:"js-serialized",value:"/*"}},clientRouting:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","clientRouting"]},valueSerialized:{type:"js-serialized",value:!0}}}}],w={configValuesSerialized:{onPrerenderStart:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+onPrerenderStart.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:T}},baseAssets:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","baseAssets"]},valueSerialized:{type:"js-serialized",value:"https://web-0nob.onrender.com"}},favicon:{type:"standard",definedAtData:{filePathToShowToUser:"/src/pages/+config.ts",fileExportPathToShowToUser:["default","favicon"]},valueSerialized:{type:"js-serialized",value:"https://web-0nob.onrender.com/assets/favicon/favicon.svg"}}}},D={...Object.assign({})};_[".page"]=D;const O={...Object.assign({})};E[".page"]=O;const k={...Object.assign({})};_[".page.server"]=k;const A={...Object.assign({})};E[".page.server"]=A;const C={...Object.assign({})};x[".page.route"]=C;const U={...Object.assign({})};E[".page.client"]=U;t({virtualFileExportsGlobalEntry:Object.freeze(Object.defineProperty({__proto__:null,neverLoaded:{},pageConfigGlobalSerialized:w,pageConfigsSerialized:z,pageFilesEager:x,pageFilesExportNamesEager:E,pageFilesExportNamesLazy:{},pageFilesLazy:_,pageFilesList:[]},Symbol.toStringTag,{value:"Module"})),assetsManifest:{
   "../../node_modules/.pnpm/vike@0.4.250_react-streaming@0.4.15_react-dom@19.2.3_react@19.2.3__react@19.2.3__vite@7_a4cba15f91f90f8df4d5529a44ec2fad/node_modules/vike/dist/client/runtime-client-routing/entry.js": {
-    "file": "assets/entries/entry-client-routing.BiYpM7Kp.js",
+    "file": "assets/entries/entry-client-routing.UhkxWyaP.js",
     "name": "entries/entry-client-routing",
     "src": "../../node_modules/.pnpm/vike@0.4.250_react-streaming@0.4.15_react-dom@19.2.3_react@19.2.3__react@19.2.3__vite@7_a4cba15f91f90f8df4d5529a44ec2fad/node_modules/vike/dist/client/runtime-client-routing/entry.js",
     "isEntry": true,
     "imports": [
-      "_chunk-C_pvf_06.js"
+      "_chunk-B68mzQ5r.js"
     ],
     "dynamicImports": [
       "virtual:vike:page-entry:client:/src/pages"
@@ -33,17 +33,17 @@ var e=Object.defineProperty,a=(a,t)=>e(a,"name",{value:t,configurable:!0});impor
     "src": "__vite-browser-external",
     "isDynamicEntry": true
   },
-  "_chunk-BQ4zIYyb.js": {
-    "file": "assets/chunks/chunk-BQ4zIYyb.js",
+  "_chunk-B68mzQ5r.js": {
+    "file": "assets/chunks/chunk-B68mzQ5r.js",
+    "name": "preload-helper"
+  },
+  "_chunk-ULetD64f.js": {
+    "file": "assets/chunks/chunk-ULetD64f.js",
     "name": "browser-ponyfill",
     "isDynamicEntry": true,
     "imports": [
       "virtual:vike:page-entry:client:/src/pages"
     ]
-  },
-  "_chunk-C_pvf_06.js": {
-    "file": "assets/chunks/chunk-C_pvf_06.js",
-    "name": "preload-helper"
   },
   "_style-052041d4.CbGtfgdF.css": {
     "file": "assets/static/style-052041d4.CbGtfgdF.css",
@@ -54,17 +54,17 @@ var e=Object.defineProperty,a=(a,t)=>e(a,"name",{value:t,configurable:!0});impor
     "src": "_style-269ba50d.D1-0B2xV.css"
   },
   "virtual:vike:page-entry:client:/src/pages": {
-    "file": "assets/entries/src_pages.CEH-hdsv.js",
+    "file": "assets/entries/src_pages.DCzqNMnh.js",
     "name": "entries/src/pages",
     "src": "virtual:vike:page-entry:client:/src/pages",
     "isEntry": true,
     "isDynamicEntry": true,
     "imports": [
-      "_chunk-C_pvf_06.js"
+      "_chunk-B68mzQ5r.js"
     ],
     "dynamicImports": [
       "__vite-browser-external",
-      "_chunk-BQ4zIYyb.js"
+      "_chunk-ULetD64f.js"
     ],
     "css": [
       "assets/static/style-052041d4.CbGtfgdF.css",
