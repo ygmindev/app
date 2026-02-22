a7abc36eb19fb263602613e1091aeef91150b4d2
YG Min <ygmindev@gmail.com>
2026-02-22 17:24:43 -0500

    commit

--- packages/lib-config-js/src/job/_job.ts
+++ packages/lib-config-js/src/job/_job.ts
@@ -4,41 +4,38 @@ import { filterNil } from '@lib/shared/core/utils/filterNil/filterNil';
 import { FREQUENCY } from '@lib/shared/datetime/datetime.models';
 
 export const _job = ({ container, env, jobs }: JobConfigModel): _JobConfigModel =>
-  jobs.map(({ command, name, schedule }) => {
-    const commandF = command ?? name;
-    return {
-      jobs: {
-        [commandF]: {
-          'runs-on': 'ubuntu-latest',
-          steps: filterNil([
-            container && {
-              name: 'sign in',
-              uses: 'docker/login-action@v2',
-              with: {
-                password: '${{ github.token }}',
-                registry: container?.server ?? '',
-                username: '${{ github.actor }}',
-              },
+  jobs.map(({ command, name, schedule }) => ({
+    jobs: {
+      [name]: {
+        'runs-on': 'ubuntu-latest',
+        steps: filterNil([
+          container && {
+            name: 'sign in',
+            uses: 'docker/login-action@v2',
+            with: {
+              password: '${{ github.token }}',
+              registry: container?.server ?? '',
+              username: '${{ github.actor }}',
             },
-            {
-              name: `run ${name}`,
-              run: container
-                ? `docker run ${env?.map((v) => `-e ${v}=\${{ secrets.${v} }}`).join(' ')} --rm ${`${container.server ?? ''}/${container.username ?? ''}/${container.image ?? ''}`} ${commandF}`
-                : commandF,
-            },
-          ]),
-        },
-      },
-
-      name,
-
-      on: {
-        schedule: [
-          {
-            cron: `${schedule.minute ?? 0} ${schedule.hour ?? 0} * * ${schedule.freq === FREQUENCY.DAILY ? '*' : FREQUENCY.WEEKLY ? (schedule.day ?? 0) : '*'}`,
           },
-        ],
-        workflow_dispatch: {},
+          {
+            name: `run ${name}`,
+            run: container
+              ? `docker run ${env?.map((v) => `-e ${v}=\${{ secrets.${v} }}`).join(' ')} --rm ${`${container.server ?? ''}/${container.username ?? ''}/${container.image ?? ''}`} ${command}`
+              : command,
+          },
+        ]),
       },
-    };
-  });
+    },
+
+    name,
+
+    on: {
+      schedule: [
+        {
+          cron: `${schedule.minute ?? 0} ${schedule.hour ?? 0} * * ${schedule.freq === FREQUENCY.DAILY ? '*' : FREQUENCY.WEEKLY ? (schedule.day ?? 0) : '*'}`,
+        },
+      ],
+      workflow_dispatch: {},
+    },
+  }));
