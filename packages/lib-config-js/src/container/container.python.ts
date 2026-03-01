e7aed0584dd3397278f66f58802c774e6099bcc2
YG Min <ygmindev@gmail.com>
2026-03-01 09:17:26 +0900

    commit

--- packages/lib-config-js/src/container/container.python.ts
+++ packages/lib-config-js/src/container/container.python.ts
@@ -2,5 +2,5 @@ import { fromConfig } from '@lib/backend/file/utils/fromConfig/fromConfig';
 import { containerConfig as configBase } from '@lib/config/container/container.base';
 
 export const containerConfig = configBase.extend(() => ({
-  dockerPathname: fromConfig('container/python'),
+  dockerPathname: fromConfig('container/python/Dockerfile'),
 }));
--- packages/lib-config-js/src/job/job.ts
+++ packages/lib-config-js/src/job/job.ts
@@ -1,5 +1,5 @@
 import { fromRoot } from '@lib/backend/file/utils/fromRoot/fromRoot';
-import { containerConfig as pythonContainerConfig } from '@lib/config/container/container.python';
+import { containerConfig as nodeContainerConfig } from '@lib/config/container/container.node';
 import { _job } from '@lib/config/job/_job';
 import { type _JobConfigModel } from '@lib/config/job/_job.models';
 import { JOB_TRIGGER } from '@lib/config/job/job.constants';
@@ -16,13 +16,13 @@ export const jobConfig = new Config<JobConfigModel, _JobConfigModel>({
       {
         commands: [
           {
-            command: 'python /app/packages/service-server-py/src/ping.py',
+            command: 'run pt',
             name: PING,
           },
         ],
         container: {
-          ...pythonContainerConfig.params(),
-          image: 'service_server',
+          ...nodeContainerConfig.params(),
+          image: 'tool_task',
           password: '${CONTAINER_PASSWORD}',
           username: '${CONTAINER_USERNAME}',
         },
@@ -35,3 +35,41 @@ export const jobConfig = new Config<JobConfigModel, _JobConfigModel>({
     version: 2.1,
   }),
 });
+
+// import { fromRoot } from '@lib/backend/file/utils/fromRoot/fromRoot';
+// import { containerConfig as pythonContainerConfig } from '@lib/config/container/container.python';
+// import { _job } from '@lib/config/job/_job';
+// import { type _JobConfigModel } from '@lib/config/job/_job.models';
+// import { JOB_TRIGGER } from '@lib/config/job/job.constants';
+// import { type JobConfigModel } from '@lib/config/job/job.models';
+// import { Config } from '@lib/config/utils/Config/Config';
+// import { FREQUENCY } from '@lib/shared/datetime/datetime.models';
+// import { PING } from '@lib/shared/http/http.constants';
+
+// export const jobConfig = new Config<JobConfigModel, _JobConfigModel>({
+//   config: _job,
+
+//   params: () => ({
+//     jobs: [
+//       {
+//         commands: [
+//           {
+//             command: 'python /app/packages/service-server-py/src/ping.py',
+//             name: PING,
+//           },
+//         ],
+//         container: {
+//           ...pythonContainerConfig.params(),
+//           image: 'service_server',
+//           password: '${CONTAINER_PASSWORD}',
+//           username: '${CONTAINER_USERNAME}',
+//         },
+//         name: PING,
+//         schedule: { freq: FREQUENCY.DAILY },
+//         trigger: JOB_TRIGGER.SCHEDULE,
+//       },
+//     ],
+//     outPathname: fromRoot('.circleci/config.yml'),
+//     version: 2.1,
+//   }),
+// });
