facdd91c52cdcf539ed33c777e64222905f6a71a
YG Min <ygmindev@gmail.com>
2026-09-16 11:20:59 -0400

    commit

--- packages/lib-ai-py/src/lib_ai/graph/utils/graph_node/graph_node.py
+++ packages/lib-ai-py/src/lib_ai/graph/utils/graph_node/graph_node.py
@@ -1,12 +1,13 @@
 # template version: 1.0.0
 
 
+from lib_shared.core.utils.base_model.base_model import BaseModel
 from lib_shared.core.utils.field.field import Field
 
 from lib_ai.agent.utils.streamable.streamable import Streamable
 
 
-class GraphNode(Streamable):
+class GraphNode[TState: BaseModel](Streamable[BaseModel]):
     name: str = Field()
 
     def edges(self) -> tuple[str, str]:
