08635de457bc63c150ac4d9696712b96fa351489
YG Min <ygmindev@gmail.com>
2026-09-16 12:41:47 -0400

    commit

--- packages/lib-ai-py/src/lib_ai/graph/utils/graph_node/graph_node.py
+++ packages/lib-ai-py/src/lib_ai/graph/utils/graph_node/graph_node.py
@@ -9,7 +9,7 @@ from lib_ai.agent.utils.streamable.streamable import Streamable
 
 class GraphNode[TState: BaseModel](Streamable[BaseModel]):
     name: str = Field()
-    messages: callable[[TState], list[str]] | None = Field(default=None)
+    descriptions: list[str] = Field(default_factory=list)
 
     def edges(self) -> tuple[str, str]:
         return (
@@ -19,3 +19,6 @@ class GraphNode[TState: BaseModel](Streamable[BaseModel]):
 
     def markup(self) -> str:
         return f'{self.name}["{self.name}"]'
+
+    def messages(self, state: TState) -> list[str] | None:
+        return None
